f9b55b133ac9edf3f556a7400d4ad097751294d6
Aaron Johnson <aaron.johnson1031@gmail.com>
2018-03-12 13:24:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, x)
-  if board[x] == " " || board[x] == ""
+  if board[x] == " " || board[x] == "" || board[x] == nil
     false
   end
 end
